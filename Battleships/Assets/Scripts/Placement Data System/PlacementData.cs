29883e77542aa6f27b6dc233260c410f97f8a8d5
Raaee <varangurenv@gmail.com>
2023-05-30 04:41:18 -0400

    Random Pawn Assignment

--- Battleships/Assets/Scripts/Placement Data System/PlacementData.cs
+++ Battleships/Assets/Scripts/Placement Data System/PlacementData.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
 /// <summary>
@@ -9,21 +8,46 @@ using UnityEngine.UI;
 /// </summary>
 public class PlacementData : MonoBehaviour
 {
-   //an array of pawns
-   public List<Pawn> pawns;
-   
+    //an array of pawns
+    public List<Pawn> pawnPrefabs; // this is the default pawn prefabs used to assign the player's army
+    public List<Pawn> pawnsInBattle; // this will be the player's pawns
+
    //enum of player or enemy 
    [SerializeField] private Team team;
 
+    private int ranNum;
+
     private bool allPawnsPlaced;
     [SerializeField] Button confirmButton;
+
+    void Start() {
+        ChooseRandomPawns(5);
+    }
 
     void Update() {
         ConfirmPlacement();
     }
 
-    public void CheckPawnPlacement() {
-        foreach (Pawn p in pawns) {
+    public void ChooseRandomPawns(int numPawns) {
+        for (int i = 0; i < numPawns; i++) {
+            ranNum = Random.Range(1, 6); // random number 1, 2, 3, 4, or 5
+            pawnsInBattle.Add(PawnPrefabOfSize(ranNum));
+          //  Debug.Log("Size: " + pawnsInBattle[i].GetPawnSize() + " / " + pawnsInBattle[i].name);
+        }
+    }
+    private Pawn PawnPrefabOfSize(int size) {
+        for (int i = 0; i < pawnPrefabs.Count; i++) {
+            if (pawnPrefabs[i].GetPawnSize() == size) {
+                return pawnPrefabs[i];
+            }
+        }
+        return null;
+    }
+
+    // checks if there is a boolean in the Pawn that is false:
+    public void CheckPawnPlacement() { 
+        // ****** MUST CHANGE FOR LOOP VARIABLE AFTER ADDING PLAYER PAWNS IN THE SCENE *****
+        foreach (Pawn p in pawnPrefabs) {
             if (p.GetPlacedStatus() == false) {
                 allPawnsPlaced = false;
                 return;
@@ -31,6 +55,7 @@ public class PlacementData : MonoBehaviour
         }
         allPawnsPlaced = true;
     }
+    // activates and deactivates the confirmation button depending on whether all pawns are placed:
     public void ConfirmPlacement() {
         CheckPawnPlacement();
         if (allPawnsPlaced) {
