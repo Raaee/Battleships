bf530d6d13e260a9ee93a05e876c22045580a7f2
PetersonNormil <petersonnormil@gmail.com>
2023-06-22 11:58:24 -0400

    fixed UI bug!

--- Battleships/Assets/Scripts/Placement Data System/PlacementData.cs
+++ Battleships/Assets/Scripts/Placement Data System/PlacementData.cs
@@ -111,6 +111,7 @@ public class PlacementData : MonoBehaviour
             }
         }
         OnAllPawnsSpawned?.Invoke();
+        Debug.Log("All pawny spawny!");
     }
     public void Placed() {
         placementConfirmed = true;
--- Battleships/Assets/Scripts/UI Scripts/AmountOfPlayerPawnsUI.cs
+++ Battleships/Assets/Scripts/UI Scripts/AmountOfPlayerPawnsUI.cs
@@ -14,11 +14,13 @@ public class AmountOfPlayerPawnsUI : MonoBehaviour
     private void Awake()
     {
         placementData.OnAllPawnsSpawned.AddListener(StartingAmountInUI);
+        Debug.Log("connected event woooo");
     }
 
     //key is pawnsize, value is the amount
     private void StartingAmountInUI()
     {
+        Debug.Log("I should be called bruh");
         sizeAmtDictionary = new Dictionary<int, int>();
         foreach (var pawns in placementData.pawnsInBattle)
         {
