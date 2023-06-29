6bbb22ae6432b7dde9c3ff8cf8c42b405e8f469f
PetersonNormil <petersonnormil@gmail.com>
2023-06-29 13:12:05 -0400

    almost finishing up refactored highlight systme

--- Battleships/Assets/Scripts/Highlighting System/InputData.cs
+++ Battleships/Assets/Scripts/Highlighting System/InputData.cs
@@ -10,31 +10,58 @@ using UnityEngine;
 /// </summary>
 public class InputData : MonoBehaviour
 {
+    [SerializeField] private GridManager playerGridmanager; 
     [Header("DEBUG")]
     [SerializeField] private CubeVisual currentCubeVisual = null;
+
+    [SerializeField] private PawnVisual currentPawnVisual = null;
     [SerializeField] private bool isDraggingPawn = false;
+    
     public List<ClickAndDrag> clickAndDrags;
 
     private void Update()
     {
         GetIsDraggingPawn();
         
-        //if you are dragging a pawn, and you are over cube visual, and its your grid, we can use the highlight system
+        //if you are dragging a pawn,
+        if (isDraggingPawn == false) return;
+        //and you are over cube visual,
+        if(currentCubeVisual == null) return;
+        //and its your grid,
+        if(DoesCubeBelongToPlayerGrid() == false) return;
+        
+        
+        //we can use the highlight system
+        ShowHighlight();
+      
+    }
+
+    private void ShowHighlight()
+    {
         //we will need the pawn size you are dragging, and the orientation 
+        PawnOrientation pawnOrientation = currentPawnVisual.GetPawnOrientation();
+        int pawnSize = currentPawnVisual.gameObject.GetComponent<Pawn>().GetPawnSize();
+        
+        //and then we just make the highlight system work its magic
+        //TODO: refactor part of the highlight so that we can call it from here
+        
     }
 
     public bool GetIsDraggingPawn()
     {
+        
         foreach (ClickAndDrag cd in clickAndDrags)
         {
             if (cd.GetIsDragging() == true)
             {
+                currentPawnVisual = cd.gameObject.GetComponent<PawnVisual>();
                 isDraggingPawn = true;
                 return true;
  
             }
         }
 
+        currentPawnVisual = null;
         isDraggingPawn = false;
         return false;
     }
@@ -51,5 +78,17 @@ public class InputData : MonoBehaviour
     public void AddSelfToClickDragList(ClickAndDrag cd)
     {
         clickAndDrags.Add(cd);
+    }
+
+    private bool DoesCubeBelongToPlayerGrid()
+    {
+        if (currentCubeVisual == null) return false;
+
+        if (playerGridmanager.GetPositionAtTile(currentCubeVisual.gameObject).x <= -1)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
