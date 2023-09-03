a77808fed3ea57e2e3361c25121c1b8e95251ac7
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2023-09-03 13:58:41 -0400

    sfx and music volume implemented

--- Battleships/Assets/Scripts/Audio Scripts/MainMenuAudio.cs
+++ Battleships/Assets/Scripts/Audio Scripts/MainMenuAudio.cs
@@ -1,12 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuAudio : MonoBehaviour
 {
     public FMODUnity.EventReference uiClickEvent;
     public FMODUnity.EventReference uiActionEvent;
 
+
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private FMOD.Studio.VCA musicVca;
+    private FMOD.Studio.VCA sfxVCA;
+
+    private const string musicVCAID = "MusicVCA";
+    private const string sfxVCAID = "SFXVCA";
+    private const string vcaPrefix = "vca:/";
+
+    private void Start()
+    {
+        musicVca = FMODUnity.RuntimeManager.GetVCA(vcaPrefix +musicVCAID);
+        sfxVCA = FMODUnity.RuntimeManager.GetVCA(vcaPrefix + sfxVCAID);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    private void SetSFXVolume(float newSfxVolume)
+    {
+        sfxVCA.setVolume(newSfxVolume);
+    }
+
+    private void SetMusicVolume(float newMusicVol)
+    {
+        musicVca.setVolume(newMusicVol);
+    }
 
     public void PlayUIClickEvent()
     {
